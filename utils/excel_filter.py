e3dc6da0328d8085d6bae5b1dc27668f02596a25
zxm <tjzxm1@gmail.com>
2023-11-22 13:04:14 +0100

    excel filter version2

--- utils/excel_filter.py
+++ utils/excel_filter.py
@@ -1,26 +1,22 @@
-#filter some 0 out
+import csv
 
-import pandas as pd
-import sys
-sys.path.append("..")
+# Load the CSV file
+file_path = 'driving_log.csv'
+with open(file_path, 'r') as f:
+    reader = csv.reader(f)
+    next(reader)  # Skip the header row
+    data = list(reader)
 
-
-# 加载Excel文件到一个DataFrame中
-file_path = 'your_excel_file.xlsx'
-df = pd.read_excel(file_path, header=None)
-print(df.shape)
-print(df.columns)
-
-# 初始化变量以跟踪连续的零值和连续零值段的起始索引
+# Initialize variables to track consecutive zeros and the start index of consecutive zero segments
 consecutive_zeros = 0
 start_index = None
 
-# 用于存储要保留的行的索引
+# A list to store the indices of the rows to keep
 rows_to_keep = []
 
-# 遍历DataFrame的行
-for index, row in df.iterrows():
-    if row[3] == 0:  # 假设第四列是0-based
+# Iterate over the rows in the data
+for index, row in enumerate(data):
+    if float(row[3]) == 0:  # Assume the fourth column is 0-based
         if consecutive_zeros == 0:
             start_index = index
         consecutive_zeros += 1
@@ -30,22 +26,26 @@ for index, row in df.iterrows():
         elif consecutive_zeros > 0 and consecutive_zeros <= 10:
             rows_to_keep.extend(range(start_index, index + 1))
         else:
-            rows_to_keep.extend([index])  # 包含非零行
+            rows_to_keep.append(index)  # Include non-zero rows
         consecutive_zeros = 0
 
-
-# 检查是否有尾随的连续零值段需要包含
+# Check if there are trailing consecutive zero segments that need to be included
 if consecutive_zeros > 10:
     rows_to_keep.extend([start_index, start_index + 1])
 
 if consecutive_zeros > 0 and consecutive_zeros <= 10:
     rows_to_keep.extend(range(start_index, index + 1))
 
-# 创建一个新的DataFrame，其中包含所选的行
-filtered_df = df.loc[rows_to_keep]
+# Create a new list with the selected rows
+filtered_data = [data[i] for i in rows_to_keep]
 
-# 将修改后的DataFrame保存到新的Excel文件中
-output_file_path = 'modified_excel_file.xlsx'
-filtered_df.to_excel(output_file_path, index=False, header=False, engine='openpyxl')
+print(f"Original data has {len(data)} rows")
+print(f"Filtered data has {len(filtered_data)} rows")
 
-print(f"已保存修改后的Excel文件至 {output_file_path}")
+# Save the modified data to a new CSV file
+output_file_path = 'modified_csv_file.csv'
+with open(output_file_path, 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerows(filtered_data)
+
+print(f"Modified CSV file has been saved to {output_file_path}")
