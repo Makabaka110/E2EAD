8906b28aeb2fc72a4e35c07e310114911ff73edb
zxm <tjzxm1@gmail.com>
2023-11-28 18:46:53 +0100

    delete all 0 filter

--- config.py
+++ config.py
@@ -7,15 +7,15 @@ import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # data type, model type AND their classes
-DATA_TYPE = 'keyboard_10min'
+DATA_TYPE = 'xbox_inverse_10min'
 MODEL_TYPE = 'ResNet50'
 
 DATA_TYPE_CLASS = SteeringDataset
 MODEL_TYPE_CLASS = SteeringModel
 
 #raw data path
-DATA_PATH = ROOT_DIR + '/data/keyboard_10min/'
-LABELS_FILE = ROOT_DIR+ '/data/keyboard_10min/driving_log.csv'
+DATA_PATH = ROOT_DIR + '/data/xbox_inverse_10min/'
+LABELS_FILE = ROOT_DIR+ '/data/xbox_inverse_10min/driving_log.csv'
 
 # features and labels path
 FEATURES = DATA_PATH + 'features.npy'
@@ -25,13 +25,13 @@ LABELS = DATA_PATH + 'labels.npy'
 MODEL_PATH = ROOT_DIR+'/models/{}/{}/'.format(DATA_TYPE,MODEL_TYPE)
 
 # load model path
-LOAD_MODEL_PATH = ROOT_DIR+'/models/{}/{}/epho_11.pth'.format(DATA_TYPE,MODEL_TYPE)
+LOAD_MODEL_PATH = ROOT_DIR+'/models/{}/{}/epho_3.pth'.format(DATA_TYPE,MODEL_TYPE)
 
 #CUDA device
 DEVICE = "cuda"
 
 # Hyperparameters
-BATCH_SIZE = 16
+BATCH_SIZE = 32
 LEARNING_RATE = 0.001
 MAX_EPOCHS = 100
 WEIGHT_DECAY = 0.0
