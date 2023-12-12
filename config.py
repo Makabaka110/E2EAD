c4d25a0e5e2794399fb518fd12f5a4ae278b452a
Wenting Miao <wenting.miao@tum.de>
2023-12-13 00:05:04 +0100

    personal config

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # data type, model type AND their classes
 
-# Data type including 'keyboard_10min', 'github_data', 'xbox_5min', 'xbox_20min'
-DATA_TYPE = 'github_data' 
+# Data type including 'keyboard_10min', 'github_data', 'xbox_5min', 'xbox_20min','github_xbox20','xbox_inverse_10min'
+DATA_TYPE = 'github_xbox20' 
 MODEL_TYPE = 'ResNet50'
 
 DATA_TYPE_CLASS = SteeringDataset
@@ -17,7 +17,7 @@ MODEL_TYPE_CLASS = SteeringModel
 
 #raw data path
 DATA_PATH = ROOT_DIR + '/data/{}/'.format(DATA_TYPE)
-LABELS_FILE = ROOT_DIR+ '/data/{}/driving_log.csv'.format(DATA_TYPE)
+LABELS_FILE = ROOT_DIR+ '/data/{}/delete_all_zero_driving_log.csv'.format(DATA_TYPE)
 
 # features and labels path
 FEATURES = DATA_PATH + 'features.npy'
@@ -27,11 +27,11 @@ LABELS = DATA_PATH + 'labels.npy'
 MODEL_PATH = ROOT_DIR+'/models/{}/{}/'.format(DATA_TYPE,MODEL_TYPE)
 
 # load drive model path
-LOAD_MODEL_PATH = ROOT_DIR+'/models/{}/{}/epho_2.pth'.format(DATA_TYPE,MODEL_TYPE)
+LOAD_MODEL_PATH = ROOT_DIR+'/models/{}/{}/epho_5.pth'.format(DATA_TYPE,MODEL_TYPE)
 
 # load train model path
 # LOAD_TRAIN_MODEL_PATH = ROOT_DIR+'/models/pretrained_model/epho_15.pth'
-
+LOAD_TRAIN_MODEL_PATH = ''
 
 #CUDA device
 DEVICE = "cuda"
